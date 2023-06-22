b7c944a0ab0cd7d1901cd0df1bb5f585526d9945
Shahriiar <bekmuhammetov.77@gmail.com>
2023-06-22 18:02:19 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,120 @@
+import java.util.Random;
+
+public class Main {
+    public static int bossHealth = 1000;
+    public static int bossDamage = 50;
+    public static String bossDefence;
+    public static String[] heroesAttackType = {"Physical", "Magical", "Mental","Lekar"};
+    public static int[] heroesHealth = {270, 260, 250 , 280};
+    public static int[] heroesDamage = {10, 15, 20 , 0};
+    public static int roundNumber = 0;
+
+    public static void main(String[] args) {
+        printStatistics();
+        while (!isGameOver()) {
+            playRound();
+        }
+    }
+
+    public static void playRound() {
+        roundNumber++;
+        chooseBossDefence();
+        bossHits();
+        lechenie();
+        heroesHit();
+        printStatistics();
+
+    }
+
+    public static void chooseBossDefence() {
+        Random random = new Random();
+        int randomIndex = random.nextInt(heroesAttackType.length - 1);
+        bossDefence = heroesAttackType[randomIndex];
+    }
+
+    public static void heroesHit() {
+        for (int i = 0; i < heroesDamage.length; i++) {
+            if (bossHealth > 0 && heroesHealth[i] > 0) {
+                int damage = heroesDamage[i];
+                if (heroesAttackType[i] == bossDefence) {
+                    Random random = new Random();
+                    int coeff = random.nextInt(9) + 2; // 2,3,4,5,6,7,8,9,10
+                    damage = heroesDamage[i] * coeff;
+                    System.out.println("Critical damage: " + damage);
+                }
+                if (bossHealth - damage < 0) {
+                    bossHealth = 0;
+                } else {
+                    bossHealth = bossHealth - damage;
+                }
+            }
+        }
+    }
+
+
+    public static void lechenie(){
+        Random random = new Random();
+        int addHealth = random.nextInt(30) + 20;
+        Random a = new Random();
+        int vbor = a.nextInt(heroesHealth.length - 1);
+        for (int i = 0; i <heroesHealth.length - 1 ; i++) {
+            if (heroesHealth[i] < 100 && heroesHealth[3] > 0){
+                int zero = vbor;
+                heroesHealth[zero] += addHealth ;
+
+            }
+        }
+
+
+    }
+
+    public static void bossHits() {
+        for (int i = 0; i < heroesHealth.length; i++) {
+            if (heroesHealth[i] > 0) {
+                if (heroesHealth[i] - bossDamage < 0) {
+                    heroesHealth[i] = 0;
+                } else {
+                    heroesHealth[i] = heroesHealth[i] - bossDamage;
+                }
+            }
+        }
+    }
+
+    public static boolean isGameOver() {
+        if (bossHealth <= 0) {
+            System.out.println("Heroes won!!!");
+            return true;
+        }
+        /*if (heroesHealth[0] <= 0 && heroesHealth[1] <= 0 && heroesHealth[2] <= 0) {
+            System.out.println("Boss won!!!");
+            return true;
+        }
+        return false;*/
+        boolean allHeroesDead = true;
+        for (int i = 0; i < heroesHealth.length; i++) {
+            if (heroesHealth[i] > 0) {
+                allHeroesDead = false;
+                break;
+            }
+        }
+        if (allHeroesDead) {
+            System.out.println("Boss won!!!");
+        }
+        return allHeroesDead;
+    }
+
+    public static void printStatistics() {
+        /*String defence;
+        if (bossDefence == null) {
+            defence = "No Defence";
+        } else {
+            defence = bossDefence;
+        }*/
+        System.out.println("ROUND " + roundNumber + " --------------------");
+        System.out.println("Boss health: " + bossHealth + " damage: " + bossDamage
+                + " defence: " + (bossDefence == null ? "No Defence" : bossDefence));
+        for (int i = 0; i < heroesHealth.length; i++) {
+            System.out.println(heroesAttackType[i] + " health: " + heroesHealth[i] + " damage: " + heroesDamage[i]);
+        }
+    }
+}
